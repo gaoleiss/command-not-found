0a24e9e9f6b9ac4dab5a377ddaec3445448c0c93
John Connolly <john@buglabs.net>
2012-02-24 16:48:00 -0500

    Initial import.

---
+++ update-from-web.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+
+set -e
+
+cd UnifiedDataExtractor
+
+# backup copy 
+if [ -e scan.data ]; then
+    cp scan.data scan.data-old
+fi
+
+# rookery has the normal archive
+wget -O scan.1 http://rookery.ubuntu.com/~mvo/command-not-found/scan.data-latest 
+# ports.ubuntu.com
+wget -O scan.2 http://ports.ubuntu.com/~mvo/command-not-found/scan.data-latest 
+
+# concat
+cat scan.1 scan.2 > scan.data
+
+# cleanup
+rm -f scan.1 scan.2
+
+if [ -e scan.data-old ]; then
+    ./diff-scan-data scan.data-old scan.data|less
+fi
